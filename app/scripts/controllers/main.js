b39afddfad66c7464f5b423bb151fe1bb429da7c
Thakkar, Aakash <Aakash_Thakkar@intuit.com>
2015-06-01 14:11:13 -0500

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name workoutCounterUiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the workoutCounterUiApp
+ */
+angular.module('workoutCounterUiApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
