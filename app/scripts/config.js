f66846260d9f69640bac70188848a193d0eb789d
Thakkar, Aakash <Aakash_Thakkar@intuit.com>
2015-06-03 16:57:55 -0500

    Adding support for Angular Bootstrap

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,7 +1,4 @@
-/**
- * Created with IntelliJ IDEA.
- * User: athakkar
- * Date: 6/2/15
- * Time: 1:29 PM
- * To change this template use File | Settings | File Templates.
- */
+angular.module('workoutCounterUiApp')
+    .constant('appConfig', {
+        url: 'http://0.0.0.0:3000/workout-counter/'
+    });
